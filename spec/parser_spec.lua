ca5e8efdff1f5bee9b5c303b49cb7077fd10e18a
Gustavo Henrique Montesião de Sousa <github@gustavo.claroenigma.com.br>
2016-07-05 15:44:15 -0300

    Update parser test to use text.lua file
    Now both parser and compiler tests are using the same document example

--- spec/parser_spec.lua
+++ spec/parser_spec.lua
@@ -76,49 +76,157 @@ se ouve palpitar um bicho.
 end)
 
 describe("A #complete definition", function()
+	local doc = loadfile("spec/text.lua")()
+
 	it("should generate the right structure", function()
-		local doc = [[
-# O monstrengo
-
-O monstrengo que está no fim do mar
-Na noite de breu ergueu-se a voar;
-À roda da nau voou três vezes,
-Voou três vezes a chiar,
-
-# Um título de seção com $fórmula = \theta^2$ incluída
-
-A força que um campo magnético aplica a uma carga movendo-se sobre ele é:
-$\mathbf{F}=q\left(\mathbf{v}\times\mathbf{B}\right)$.]]
-
 		local expected = {
-			{"section", {
-				{"plaintext", "O monstrengo"}
-			}},
-			{"paragraph", {
-				{"plaintext",
-					"O monstrengo que está no fim do mar\n" ..
-					"Na noite de breu ergueu-se a voar;\n" ..
-					"À roda da nau voou três vezes,\n" ..
-					"Voou três vezes a chiar,"
-				}
-			}},
-			{"section", {
-				{"plaintext", "Um título de seção com "},
-				{"inlinemath", [[$fórmula = \theta^2$]]},
-				{"plaintext", " incluída"}
-			}},
-			{"paragraph", {
-				{"plaintext", "A força que um campo magnético aplica a uma carga movendo-se sobre ele é:\n"},
-				{"inlinemath", [[$\mathbf{F}=q\left(\mathbf{v}\times\mathbf{B}\right)$]]},
-				{"plaintext", "."}
-			}}
+			{"section",
+				{"plaintext", "(hrtf) Auralização e o emprego de HRTFs"}
+			},
+
+			{"paragraph",
+				{"plaintext", [=[
+Além dessas alterações na própria estrutura da onda sonora, o ouvinte capta um
+outro tipo de informação decorrente do deslocamento do sinal sonoro através do
+espaço de propagação: as diferenças de fase (chamadas ITD, do inglês
+*Interaural Time Difference*) e de intensidade (chamadas ILD, do inglês
+*Interaural Level Difference*) dos sinais que chegam a seu ouvido
+direito e esquerdo. A inserção artificial desses efeitos em um sinal sonoro,
+com o intuito de reconstruir ou modelar uma cena sonora real, é chamada
+auralização [@alton2000master @michael2007auralization].]=]}
+			},
+
+			{"paragraph",
+				{"plaintext", [=[
+Pensando em termos de processamento de sinais, uma HRTF é, idealmente, um
+filtro %--- cuja função de transferência é, digamos, ]=]},
+
+				{"inlinemath", "$H(z)$"},
+
+				{"plaintext", [=[ --- 
+que modifica um sinal de entrada ]=]},
+
+				{"inlinemath", "$x(n)$"},
+
+				{"plaintext", [=[ incutindo-lhe as mesmas transformações
+supracitadas, de modo a tornar a saída ]=]},
+
+				{"inlinemath", "$y(n)$"},
+
+				{"plaintext", [=[ igual ao
+sinal sonoro que chega ao ouvido.
+Sendo ]=]},
+
+				{"inlinemath", "$h(n)$"},
+
+				{"plaintext", [=[ a resposta impulsiva desse filtro, a saída desejada será, então,
+calculada através da convolução do sinal de entrada com ]=]},
+
+				{"inlinemath", "$h(n)$"},
+
+				{"plaintext", ", definida por:"}
+			},
+
+			{"paragraph",
+				{"inlinemath", "$$"},
+
+			    {"inlinemath", [=[
+$ (convolução)
+y(n) = (x*h)(n) = \sum_k x(k)h(n-k).
+$]=]},
+
+				{"inlinemath", "$$"},
+			},
+
+			{"section", {"plaintext", "(cipic) O banco de dados CIPIC"}},
+
+			{"paragraph",
+				{"plaintext", [=[
+As HRTFs foram medidas tendo os indivíduos se posicionado no centro de uma
+esfera de 1~m de raio com diâmetro alinhado com o eixo interauricular do
+indivíduo (cf. figura #esfera-do-cipic). Alto-falantes de 5,8~cm de raio
+foram colocados em diversas posições ao longo da esfera (aproximadamente nos
+pontos exibidos na figura #esfera-do-cipic). Os canais auditivos dos
+indivíduos foram bloqueados e microfones foram utilizados para captar o sinal
+emitido pelos alto-falantes.]=]}
+			},
+
+			{"section", {"plaintext", "(resultados) Resultados"}},
+
+			{"paragraph",
+				{"plaintext", [=[
+Nenhum dos métodos encontrados na literatura para se obterem filtros IIR a
+partir de filtros FIR impõe restrições ao número de pólos e zeros reais que
+produz. Assim, não se pode garantir que todos os filtros IIR do banco de
+dados terão o mesmo número de pólos reais e de zeros reais, qualquer que seja a
+ténica usada para gerar esses filtros IIR. E o que se observou, ao implementar
+o método de Kalman, é que, de fato, os números de pólos e de zeros reais
+variam filtro a filtro. A tabela #tab:pzreais mostra a distribuição de
+cada configuração entre os filtros do banco obtidos pelo método de Kalman, e a
+figura #fig:pzreais apresenta graficamente a distribuição espacial dos
+filtros com configuração mais comum.]=]},
+			},
+
+			{"paragraph", {"plaintext", [=[
+%{
+%	{
+%		| polos reais | zeros reais | total de filtros |
+%		|-------------|-------------|------------------|
+%		| 0           | 0           | 177              |
+%		| 0           | 2           | 826              |
+%
+%		(tab:pzreais-6) Filtros com 6 polos e 6 zeros
+%	},
+%	{
+%		| polos reais | zeros reais | total de filtros |
+%		|-------------|-------------|------------------|
+%		| 0           | 0           | 49               |
+%		| 0           | 2           | 306              |
+%
+%		(tab:pzreais-8) Filtros com 8 polos e 8 zeros
+%	};
+%	{
+%		| polos reais | zeros reais | total de filtros |
+%		|-------------|-------------|------------------|
+%		| 0           | 0           | 2                |
+%		| 0           | 4           | 174              |
+%
+%		(tab:pzreais-10) Filtros com 10 polos e 10 zeros
+%	},
+%	{
+%		| polos reais | zeros reais | total de filtros |
+%		|-------------|-------------|------------------|
+%		| 0           | 2           | 53               |
+%		| 0           | 4           | 52               |
+%
+%		(tab:pzreais-12) Filtros com 12 polos e 12 zeros
+%	}
+%
+%	(tab:pzreais) Distribuição dos filtros segundo número de polos e de zeros reais, para
+%	filtros de algumas ordens diferentes.
+%}
+%
+%]=]},
+				{"inlinemath", "$$"},
+			
+				{"inlinemath", [=[$
+%{
+%	| f(x) = { | 1, x < 3; |
+%    |          | 0, c.c.   |
+%}
+%$]=]},
+
+				{"inlinemath", "$$"},
+
+				{"plaintext", "\n"}
+			}
 		}
 
 		local processors = {}
 		processors.inlinemath = function(text) return {"inlinemath", text} end
 		processors.plaintext = function(text) return {"plaintext", text} end
-		processors.paragraph = function(...) return {"paragraph", {...}} end
-		processors.section = function(...) return {"section", {...}} end
+		processors.paragraph = function(...) return {"paragraph", ...} end
+		processors.section = function(...) return {"section", ...} end
 
 		local result = parser.parse(doc, processors)
 		assert.are.same(expected, result)
